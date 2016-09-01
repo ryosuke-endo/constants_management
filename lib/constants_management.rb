fb7d709c60172c5bf287d3028d515c016166c53d
ryosuke-endo <frozen_faithjp2@yahoo.co.jp>
2016-09-01 17:44:44 +0900

    リファクタリング

--- lib/constants_management.rb
+++ lib/constants_management.rb
@@ -1,25 +1,23 @@
 require 'yaml'
-require 'open-uri'
 
 class ConstantsManagement
-  def initialize
-    date = YAML.load(File.open("spec/sample.yml"))
-    date.each do |key, value|
+  def initialize(source = self.class.source)
+    create_const(YAML.load(File.open(source)))
+  end
+
+  def create_const(date)
+    date.each do |klass, value|
       name = value.keys.first.upcase
       const = value.values.first
-      if const.is_a?(Array)
-        eval <<-EOS
-          class #{key}
-            #{name} = #{const}
-          end
-        EOS
-      else
-        eval <<-EOS
-          class #{key}
-            #{name} = "#{const}"
-          end
-        EOS
-      end
+      self.class.const_set(klass, Class.new { |k|
+        const_set(name, const)
+      })
+    end
+  end
+
+  class << self
+    def source(value = nil)
+      @source ||= value
     end
   end
 end
